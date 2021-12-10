551edc973a0a9975657dd569d4596eef2219db76
akmalzakia <zaki.19051@mhs.its.ac.id>
2021-12-10 19:09:22 +0700

    forget to extend easyWorld to World

--- src/World/easyWorld.js
+++ src/World/easyWorld.js
@@ -1,21 +1,14 @@
-import { createCamera } from "../../components/camera.js";
-import { createLights } from "../../components/lights.js";
-import { createScene, setPanorama } from "../../components/scene.js";
-import { createRenderer } from "../../systems/renderer.js";
-import { createLoadingManager } from "../../systems/loadingManager.js";
-
-import { Loop } from "../../systems/Loop.js";
-import { Resizer } from "../../systems/Resizer.js";
-import { createControls } from "../../systems/controls.js";
+import { World } from "./World.js";
 import { Plane } from "../../components/Plane.js";
 import { BasicEnemy } from "../../components/BasicEnemy.js";
-import { Grid } from "../../systems/Grid.js";
+import { createLights } from "../../components/lights.js";
+import { HardEnemy } from "../../components/HardEnemy.js";
 import { Spawner } from "../../components/Spawner.js";
 import { Wave } from "../../components/Wave.js";
 import { score, scoreboard, nilai} from "../../components/score.js";
 
 
-class easyWorld {
+class easyWorld extends World{
     // Setup your game here
 
     constructor(canvas, sizes){
@@ -34,8 +27,8 @@ class easyWorld {
         });
         this.scene.collidableObject.push(plane);
 
-        const spawner = new Spawner(this.loop, grid);
-        spawner.initializeModel(grid.gridSpace);
+        const spawner = new Spawner(this.loop, this.grid);
+        spawner.initializeModel(this.grid.gridSpace);
         this.loop.updatables.push(spawner);
 
         nilai.innerHTML = score.value;
